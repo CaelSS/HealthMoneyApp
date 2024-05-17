a72cf99406d8c21ffbbd773d98606984dea297b7
ca_el <caelsantos71@gmail.com>
2024-05-17 14:48:18 -0300

    Correção de bugs e adicionar evento de botao para tela principal do admin

--- app/src/main/java/com/example/healthmoneyapp/MainActivity.java
+++ app/src/main/java/com/example/healthmoneyapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.healthmoneyapp;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,12 +15,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-
+import com.example.healthmoneyapp.database.DataBase;
+import com.example.healthmoneyapp.database.Usuario;
 
 
 public class MainActivity extends AppCompatActivity {
     TextView txtCadastrar;
+    Button botaoEntrar,botaoSair;
+    EditText editLogin, editSenha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,13 +36,83 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });*/
 
+        //CAPTURO OS COMPONENTES DA TELA MAIN
 
         txtCadastrar = findViewById(R.id.txtCadastrar);
+        editLogin = findViewById(R.id.editLogin);
+        editSenha = findViewById(R.id.editSenha);
+        botaoEntrar = findViewById(R.id.botãoEntrar);
+        botaoSair = findViewById(R.id.botãoSair);
+
+        //ADICIONO OS EVENTOS DE BOTAO DA TELA DE LOGIN
+
+        //chama a tela de cadastrar quando o  texto é pressionado
         txtCadastrar.setOnClickListener((View view)->{
             Intent intent = new Intent(MainActivity.this, TelaCadastro.class);
             startActivity(intent);
         });
+        botaoSair.setOnClickListener((View view)->{
+            finishAndRemoveTask(); // finaiza a task, sem matar o processo
+        });
+        botaoEntrar.setOnClickListener((View view)->{ // FORMA MODERNA - LAMBDA FUNCTION
+            logar();
+        });
+
+    }
 
 
+    private void logar() {
+        // Verifica se os campos foram preenchidos
+        if (!validarCampos()) {
+            return;
+        }
+        // Capturar os textos dos componentes
+        String login = editLogin.getText().toString();
+        String senha = editSenha.getText().toString();
+
+        // Verifica se o login é válido
+        if (!validarLogin(login, senha)) {
+            showAlertDialog(this, "Erro de Login", "Dados de login incorretos");
+            return;
+        }
+        // Muda a tela caso os dados estejam corretos e vai pra tela de admin
+        Intent intent = new Intent(this, TelaAdministrador.class);
+        startActivity(intent);
+    }
+
+    private boolean validarCampos() {
+        if (editLogin.getText().toString().trim().equals("")) {
+            editLogin.setError("Não vai entrar sem login!");
+            return false;
+        }
+
+        if (editSenha.getText().toString().trim().equals("")) {
+            editSenha.setError("E a senha, qual é?");
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validarLogin(String login, String senha) {
+        DataBase db = Room.databaseBuilder(getApplicationContext(), DataBase.class, "Base de Dados")
+                .allowMainThreadQueries().build();
+        Usuario usuario = db.getUserDao().getUserLogin(login, senha);
+
+        return usuario != null;
+    }
+
+    private void showAlertDialog(Context context, String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
